cb6d73ea3508f3be9307db522efb40b1ce2e310a
Harald Kirschner <hkirschner@microsoft.com>
2023-05-16 05:03:36 +0000

    openapi yaml out of the bxx

--- routers/wellknown.py
+++ routers/wellknown.py
@@ -1,9 +1,27 @@
+import yaml
+import json
 from string import Template
 from fastapi import APIRouter, Request
 from fastapi.responses import FileResponse, Response
 
 wellknown = APIRouter(prefix="/.well-known", tags=["well-known"])
 
+@wellknown.get("/openapi.yaml", include_in_schema=False)
+async def openapi_yaml(request: Request):
+    openapi = request.app.openapi()
+    # Add public server URL
+    host_header = request.headers.get("X-Forwarded-Host") or request.headers.get("Host")
+    protocol = request.headers.get("X-Forwarded-Proto") or request.url.scheme
+    openapi["servers"] = [{"url": f"{protocol}://{host_header}"}]
+    # Use title and description from ai-plugin spec
+    with open(".well-known/ai-plugin.json", encoding="utf-8") as file:
+        ai_plugin = json.loads(file.read())
+        openapi["info"]["title"] = ai_plugin["name_for_human"]
+        openapi["info"]["description"] = ai_plugin["description_for_human"]
+        return Response(
+            content=yaml.dump(openapi),
+            media_type="text/vnd.yaml",
+        )
 
 @wellknown.get("/logo.png", include_in_schema=False)
 async def logo():
