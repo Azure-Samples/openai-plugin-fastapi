5a64de2cd072ef14ef23be0aad00a10f4dbec941
Harald Kirschner <hkirschner@microsoft.com>
2023-05-16 23:04:12 +0000

    More from-scratch cleanup

--- main.py
+++ main.py
@@ -2,5 +2,17 @@ from fastapi import FastAPI
 from routers.wellknown import wellknown
 import json
 
-app = FastAPI(title="Product Search", version="1.0")
+app = FastAPI()
 app.include_router(wellknown)
+
+# load products from data
+with open('data/products.json') as f:
+    products = json.load(f)
+
+# /products?query=list-of-keywords
+@app.get("/products")
+async def get_products(query: str = None):
+    if query:
+        query = query.lower()
+        return [product for product in products if query in product['description'].lower()]
+    return products
