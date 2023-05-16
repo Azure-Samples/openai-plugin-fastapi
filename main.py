fabe31bc4beee4ce18b9c3dbf25a128e28f72fc0
Harald Kirschner <hkirschner@microsoft.com>
2023-05-16 01:18:04 +0000

    Taking models out for now

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from routers.wellknown import wellknown
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List, Optional
-from pydantic import BaseModel
 import json
 
 app = FastAPI(title="Product Search", version="1.0")
@@ -12,18 +10,8 @@ app.add_middleware(CORSMiddleware, allow_origins=["https://chat.openai.com"])
 with open("./data/products.json", "r") as f:
     products = json.load(f)
 
-class Product(BaseModel):
-    name: str
-    description: str
-    category: str
-    size: str
-    price: float
-
-class ProductQuery(BaseModel):
-    query: Optional[str] = None
-
-@app.get("/products", response_model=List[Product], summary="Get a list of products", operation_id="getProducts")
-async def get_products(query: Optional[str] = None):
+@app.get("/products", summary="Get a list of products", operation_id="getProducts")
+async def get_products(query: str = None):
     """
     Returns a list of products, optionally filtered by providing a query parameter.
     """
