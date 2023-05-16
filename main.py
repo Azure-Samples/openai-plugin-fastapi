03a775017f7d9356a11763ca4ee9efd86d9a1596
Harald Kirschner <hkirschner@microsoft.com>
2023-05-16 00:44:42 +0000

    More FastAPI-ish main for a better OpenAPI

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from fastapi import FastAPI
 from routers.wellknown import wellknown
 from fastapi.middleware.cors import CORSMiddleware
-
+from typing import List, Optional
+from pydantic import BaseModel
 import json
 
-app = FastAPI()
+app = FastAPI(title="Product Search", version="1.0")
 app.include_router(wellknown)
 app.add_middleware(CORSMiddleware, allow_origins=["https://chat.openai.com"])
 
-# Load fake products from product.json
 with open("./data/products.json", "r") as f:
     products = json.load(f)
 
+class Product(BaseModel):
+    name: str
+    description: str
+    category: str
+    size: str
+    price: float
 
-@app.get("/products")
-async def get_products(query: str = None):
-    """Get products from the fake database"""
+class ProductQuery(BaseModel):
+    query: Optional[str] = None
+
+@app.get("/products", response_model=List[Product], summary="Get a list of products", operation_id="getProducts")
+async def get_products(query: Optional[str] = None):
+    """
+    Returns a list of products, optionally filtered by providing a query parameter.
+    """
     if query:
-        return [
-            product for product in products if query.lower() in product["description"]
-        ]
+        return [product for product in products if query.lower() in str(product.values()).lower()]
     return products
