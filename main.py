178a3e124c6d5e504182b24f03710354511752d1
Harald Kirschner <hkirschner@microsoft.com>
2023-05-17 14:25:16 +0000

    Revert scratch code

--- main.py
+++ main.py
@@ -4,15 +4,3 @@ import json
 
 app = FastAPI()
 app.include_router(wellknown)
-
-# load products from data
-with open('data/products.json') as f:
-    products = json.load(f)
-
-# /products?query=list-of-keywords
-@app.get("/products")
-async def get_products(query: str = None):
-    if query:
-        query = query.lower()
-        return [product for product in products if query in product['description'].lower()]
-    return products
